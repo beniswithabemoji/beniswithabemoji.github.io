0354ef66f7e8dca2e766dd2cd739d987853e62f0
beniswithabemoji <70650893+beniswithabemoji@users.noreply.github.com>
2020-12-02 20:17:35 -0600

    g
    g

--- assets/js/_utils/timeago.js
+++ assets/js/_utils/timeago.js
@@ -8,76 +8,71 @@
 
 $(function() {
 
-  var toRefresh = $(".timeago").length;
+    function timeago(date, isLastmod) {
+        var now = new Date();
+        var past = new Date(date);
+        var seconds = Math.floor((now - past) / 1000);
 
-  var intervalId = void 0;
+        var year = Math.floor(seconds / 31536000);
+        if (year >= 1) {
+            return year + " year" + (year > 1 ? "s" : "") + " ago";
+        }
 
-  function timeago(iso, isLastmod) {
-    let now = new Date();
-    let past = new Date(iso);
+        var month = Math.floor(seconds / 2592000);
+        if (month >= 1) {
+            return month + " month" + (month > 1 ? "s" : "") + " ago";
+        }
 
-    if (past.getFullYear() !== now.getFullYear()) {
-      toRefresh -= 1;
-      return past.toLocaleString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "numeric"
-      });
+        var week = Math.floor(seconds / 604800);
+        if (week >= 1) {
+            return week + " week" + (week > 1 ? "s" : "") + " ago";
+        }
+
+        var day = Math.floor(seconds / 86400);
+        if (day >= 1) {
+            return day + " day" + (day > 1 ? "s" : "") + " ago";
+        }
+
+        var hour = Math.floor(seconds / 3600);
+        if (hour >= 1) {
+            return hour + " hour" + (hour > 1 ? "s" : "") + " ago";
+        }
+
+        var minute = Math.floor(seconds / 60);
+        if (minute >= 1) {
+            return minute + " minute" + (minute > 1 ? "s" : "") + " ago";
+        }
+
+        return (isLastmod ? "just" : "Just") + " now";
     }
 
-    if (past.getMonth() !== now.getMonth()) {
-      toRefresh -= 1;
-      return past.toLocaleString("en-US", {
-        month: "short",
-        day: "numeric"
-      });
+
+    function updateTimeago() {
+        $(".timeago").each(function() {
+            if ($(this).children("i").length > 0) {
+                var isLastmod = $(this).hasClass('lastmod');
+                var node = $(this).children("i");
+                var date = node.text(); /* ISO Dates: 'YYYY-MM-DDTHH:MM:SSZ' */
+                $(this).text(timeago(date, isLastmod));
+                $(this).append(node);
+            }
+        });
+
+        if (vote == 0 && intervalId != undefined) {
+            clearInterval(intervalId); /* stop interval */
+        }
+        return vote;
     }
 
-    let seconds = Math.floor((now - past) / 1000);
 
-    let day = Math.floor(seconds / 86400);
-    if (day >= 1) {
-      toRefresh -= 1;
-      return day + " day" + (day > 1 ? "s" : "") + " ago";
+    var vote = $(".timeago").length;
+    if (vote == 0) {
+        return;
     }
 
-    let hour = Math.floor(seconds / 3600);
-    if (hour >= 1) {
-      return hour + " hour" + (hour > 1 ? "s" : "") + " ago";
+    if (updateTimeago() > 0) { /* run immediately */
+        vote = $(".timeago").length; /* resume */
+        var intervalId = setInterval(updateTimeago, 60000); /* loop every minutes */
     }
-
-    let minute = Math.floor(seconds / 60);
-    if (minute >= 1) {
-      return minute + " minute" + (minute > 1 ? "s" : "") + " ago";
-    }
-
-    return (isLastmod ? "just" : "Just") + " now";
-  }
-
-  function updateTimeago() {
-    $(".timeago").each(function() {
-      if ($(this).children("i").length > 0) {
-        var basic = $(this).text();
-        var isLastmod = $(this).hasClass("lastmod");
-        var node = $(this).children("i");
-        var date = node.text(); /* ISO Date: "YYYY-MM-DDTHH:MM:SSZ" */
-        $(this).text(timeago(date, isLastmod));
-        $(this).append(node);
-      }
-    });
-
-    if (toRefresh === 0 && typeof intervalId !== "undefined") {
-      clearInterval(intervalId); /* stop interval */
-    }
-    return toRefresh;
-  }
-
-  if (toRefresh === 0) {
-    return;
-  }
-
-  if (updateTimeago() > 0) { /* run immediately */
-    intervalId = setInterval(updateTimeago, 60000); /* run every minute */
-  }
 
 });
